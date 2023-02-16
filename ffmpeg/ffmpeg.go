c8da1856c03447e4003ffd73eccba564c9f3f1a6
root <root@LAPTOP-8S0OJK5R>
2023-02-16 17:12:14 +0700

    update

--- ffmpeg/ffmpeg.go
+++ ffmpeg/ffmpeg.go
@@ -14,8 +14,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/floostack/transcoder"
-	"github.com/floostack/transcoder/utils"
+	"github.com/hieutm1/gotranscoder"
+	"github.com/hieutm1/gotranscoder/utils"
 )
 
 // Transcoder ...
